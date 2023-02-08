f1fdd964d229d8f9fc5c45c1d532d4eaf848b8c2
Your Name <chdharma412@gmail.com>
2023-02-08 22:25:21 +0530

    added new files

--- Functions/CallBy_Value_Reference.py
+++ Functions/CallBy_Value_Reference.py
@@ -12,3 +12,13 @@ mylist = [10,20,30,40]
 
 add_more(mylist)
 print("Outside Function:", mylist)
+
+
+str1="teja"
+print(id(str1))
+
+list1=[1,2,3,4]
+print(id(list1))
+#2141474045056
+list2=list1.append(56)
+print(id(list2))
---
+++ Functions/module2.py
@@ -0,0 +1,8 @@
+from module import *
+
+
+
+result=sum(8,6)
+print(result)
+result1=subtr(74,25)
+print(result1)
