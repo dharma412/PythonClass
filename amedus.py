7ec5e5bef89ca6b528a37a8a88e788bf20269b27
Dharmateja <chdharma412@gmail.com>
2023-07-12 14:57:32 +0530

    interview questions

---
+++ amedus.py
@@ -0,0 +1,258 @@
+Pen
+with cap -
+
+functional:
+1.
+Pen is able
+write if riffle
+should
+have
+air
+pressure
+to
+push
+ink.
+
+2. if ball
+point
+of
+pen is damge
+link
+should
+not come
+out
+of
+the
+riffle.
+
+2.
+ink
+should
+not come
+out
+when
+not using
+pen.
+
+150 -
+
+non
+ft -
+riffle
+has
+certain
+capcity
+to
+hold
+ink
+more.
+Pen
+body
+should
+not brekable
+even
+after
+multiple
+thorows
+
+functional - -- (regression - -sanity is subset regression
+smoke)
+
+list1 = ["ahello", "are", "how"]
+list2 = ['a', 'r', 'e']
+
+count = 0
+for i in range(len(list1)):
+    for j in range(0, i)
+        if list2[j] in str(list1[i]):
+            count = count + 1
+
+        else:
+            count = 0
+
+if count == len(list2):
+    print()
+
+str2 = "alpha6%46837Ju97*&^"
+
+pattern = "\D|\W"
+
+result = re.findall(pattern, str2)
+
+this is firstline
+this is second
+lines
+
+content = "this is firstline\n this is second lines"
+
+str2 = "this is word and word is this "
+
+with open(file.txt, 'r') as f1:
+    content = f1.read()
+
+list1 = content.split(' ')
+list1 = ["this,"this", "word, "is", "word"]
+
+di = {}
+
+for i in list1:
+    if i in di.keys()
+        d[i] = di[i] + 1
+
+    else:
+        di[i] = 1
+
+print(di)
+
+inner
+join
+
+table
+1
+employee:
+
+table
+2
+customer: city
+
+slect
+employee.empl_name, customer.city
+from employee LEFT
+
+JOIN
+customer
+where
+employee.name = customer.customername
+
+act12345
+
+192.168
+.0
+.1
+
+if the sum of digits equals the product of individual digits in a number, it is a spy.
+Check if the
+number
+given as an
+argument is a
+Spy
+Number or not, an
+use
+only
+1
+loop.(Please
+do
+not use
+more
+that
+one
+loop in the
+script,)
+The
+should
+not also
+use
+explicit
+datatype
+conversion
+like
+converting
+number
+to
+string and again
+from string to
+
+int
+
+123
+
+
+def spy(num):
+    sum = 0
+    prod = 1
+    while num != 0:
+        sum = sum + num % 10
+        prod = prod * (num % 10)
+        n = n // 10
+    if sum == prod:
+        print("it is spy")
+
+
+spy(123)
+
+str1 = "abcjw:, .@! eiw"
+
+import re
+
+patter = "[a=z][A-Z]"
+
+result = re.findall(patter, str1
+")
+
+{
+    "glossary": {
+        "title": "example glossary",
+        "GlossDiv": {
+            "title": "S",
+            "GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+                    "SortAs": "SGML",
+                    "GlossTerm": "Standard Generalized Markup Language",
+                    "Acronym": "SGML",
+                    "Abbrev": "ISO 8879:1986",
+                    "GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+                        "GlossSeeAlso": [
+                            "GML",
+                            "XML"
+                        ]
+                    },
+                    "GlossSee": "markup"
+                }
+            }
+        }
+    }
+}
+
+FETCH
+detailas
+Create
+Session
+aliasname
+URL
+${response}
+PATH
+ON
+Session
+ALISANAME
+URL / ORDER('67467')
+Set
+Test
+Variable  ${Response}   ${response}
+
+Validate
+JSON
+FETCH
+detailas
+Set
+Variable  ${Response}     ${response.json()}
+${response_code}
+get
+status
+code   ${response}
+should
+be
+eqaul   ${response_code}
+200
+${values}    ${response}
+get
+value
+from json   $.GlossDef.GlossSeeAlso
+${len}
+Get
+Length  ${values}
+should
+be
+eqaul   ${len}
+2
