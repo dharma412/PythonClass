a3314c712feb074273ee52ce310f32c64eabe73b
Dharma Teja Chaluvadi <dharmachaluadi.teja@amadeus.com>
2024-12-19 22:04:32 +0530

    API Testcases

--- 17_SeleniumCLass/Login/Locators.py
+++ 17_SeleniumCLass/Login/Locators.py
@@ -2,11 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdrivermanager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
-service = Service(ChromeDriverManager().download())
-driver = webdriver.Chrome(service=service)
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager(url='https://storage.googleapis.com/chrome-for-testing-public/131.0.6778.108/win64/chromedriver-win64.zip').install()))
 
 driver.get("http://www.google.com")
 driver.maximize_window()
