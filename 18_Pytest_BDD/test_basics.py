129ecf7163df68fba6cd150966d3edc4b69df049
Dharma Teja Chaluvadi <dharmachaluadi.teja@amadeus.com>
2024-12-22 22:55:48 +0530

    API Testcases

--- 18_Pytest_BDD/test_basics.py
+++ 18_Pytest_BDD/test_basics.py
@@ -1,10 +1,18 @@
+import pytest
 
 
+@pytest.mark.regg
 def test_test1():
     print("This is first testcase")
 
+@pytest.mark.smoke
 def test_test2():
     print("This is first testcase")
 
+@pytest.mark.sanity
+@pytest.mark.smoke
 def test_test3():
     print("This is first testcase")
+
+def tes_quality():
+    assert 10 == 11
