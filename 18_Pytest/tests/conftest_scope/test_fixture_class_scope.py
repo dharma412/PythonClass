d7e3cd74ab3cea53d361ad768ee440bd46699910
Your Name <chdharma412@gmail.com>
2025-01-03 16:24:06 +0530

    pytest testcases

--- 18_Pytest/tests/conftest_scope/test_fixture_class_scope.py
+++ 18_Pytest/tests/conftest_scope/test_fixture_class_scope.py
@@ -2,15 +2,19 @@ import pytest
 from selenium.webdriver.common.by import By
 
 
+
 @pytest.mark.usefixtures('driver_creation')
-class TestExample:
-    def test_case1(self, driver_creation):
-        driver=driver_creation
-        driver.get('https://www.sbicard.com/')
-        print(driver.title)
+class Test_Example:
 
-    def test_case2(self, driver_creation):
-        driver = driver_creation
-        driver.get('https://www.sbicard.com/')
-        driver.find_element(By.XPATH,'//a[contains(text(),"Corporate")]')
+    def setup_method(self,method,driver_creation):
+        self.driver=driver_creation
+        print(type(self.driver))
 
+    def test_case1(self):
+        #driver=driver_creation
+        self.driver.get('https://www.sbicard.com/')
+        print(self.driver.title)
+
+    def test_case2(self):
+        self.driver.get('https://www.sbicard.com/')
+        self.driver.find_element(By.XPATH,'//a[contains(text(),"Corporate")]')
